3cf59cffb222d531f2c320ddb1a3d38744840e76
EmiyaKurou001 <vudangkhoa922308@gmail.com>
2025-03-09 21:52:01 +0700

    Code demo

---
+++ HEALTH_SUPPORT.Services/IServices/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEALTH_SUPPORT.Services.IServices
+{
+    public interface IOrderService
+    {
+    }
+}
---
+++ HEALTH_SUPPORT.Services/RequestModel/OrderRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEALTH_SUPPORT.Services.RequestModel
+{
+    public static class OrderRequest
+    {
+        public class CreateOrderModel
+        {
+            public Guid SubscriptionId { get; set; }
+            public Guid AccountId { get; set; }
+
+            [Required]
+            [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1.")]
+            public int Quantity { get; set; }
+        }
+    }
+}
