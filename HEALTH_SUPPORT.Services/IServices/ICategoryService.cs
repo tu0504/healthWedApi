d1961ab0494bc5a897cdafd5e51ee7f9d83a6b58
EmiyaKurou001 <vudangkhoa922308@gmail.com>
2025-03-15 17:12:36 +0700

    Adding functions for category

---
+++ HEALTH_SUPPORT.Services/IServices/ICategoryService.cs
@@ -0,0 +1,19 @@
+﻿using HEALTH_SUPPORT.Services.RequestModel;
+using HEALTH_SUPPORT.Services.ResponseModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEALTH_SUPPORT.Services.IServices
+{
+    public interface ICategoryService
+    {
+        Task<List<CategoryResponse.GetCategoryModel>> GetCategory();
+        Task<CategoryResponse.GetCategoryModel?> GetCategoryById(Guid id);
+        Task AddCategory(CategoryRequest.CreateCategoryModel model);
+        Task UpdateCategory(Guid id, CategoryRequest.UpdateCategoryModel model);
+        Task RemoveCategory(Guid id);
+    }
+}
