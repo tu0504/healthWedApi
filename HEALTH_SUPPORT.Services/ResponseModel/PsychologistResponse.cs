9db2f3562cb7811cbf7f82737c1031af39f00b39
EmiyaKurou001 <vudangkhoa922308@gmail.com>
2025-03-16 00:22:40 +0700

    Update PsychologistResponse.cs

--- HEALTH_SUPPORT.Services/ResponseModel/PsychologistResponse.cs
+++ HEALTH_SUPPORT.Services/ResponseModel/PsychologistResponse.cs
@@ -7,16 +7,15 @@ using System.Threading.Tasks;
 
 namespace HEALTH_SUPPORT.Services.ResponseModel
 {
-    public class PsychologistResponse
+    public static class PsychologistResponse
     {
-        public class GetPsychologistModel
-        {
-            public Guid Id { get; set; }
-            public string Name { get; set; }
-            public string Email { get; set; }
-            public string PhoneNumber { get; set; }
-            public string Specialization { get; set; }
-            public bool IsDelete { get; set; }
-        }
+        public record GetPsychologistsModel(
+            Guid Id,
+            string Name,
+            string Email,
+            string PhoneNumber,
+            string Specialization,
+            DateTimeOffset CreateAt
+        );
     }
 }
